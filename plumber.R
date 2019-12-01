1f8eaaee013c1e9a3e3d546fd98ebfc8bd21bc9e
edavidaja <enyinnaya.aja@gmail.com>
2019-11-30 20:51:33 -0500

    added response for OPTIONS

--- plumber.R
+++ plumber.R
@@ -1,7 +1,17 @@
 #' @filter cors
-cors <- function(res) {
+cors <- function(req, res) {
+
   res$setHeader("Access-Control-Allow-Origin", "*")
-  plumber::forward()
+
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
+
 }
 
 #' @get /
